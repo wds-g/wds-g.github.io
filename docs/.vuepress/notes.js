de079eab0114870f480945bbf451ccd5e1cbac7c
祝辉 王 <xiluofu@163.com>
2024-09-25 22:32:43 +0800

    📒保持原生WPF控件样式

--- docs/.vuepress/notes.js
+++ docs/.vuepress/notes.js
@@ -1,13 +1,19 @@
-import { defineNoteConfig, defineNotesConfig } from 'vuepress-theme-plume'
-
-const demoNote = defineNoteConfig({
-  dir: 'demo',
-  link: '/demo',
-  sidebar: ['', 'foo', 'bar'],
+import {devexpresswpf,defineNotesConfig } from 'vuepress-theme-plume'
+/**
+ * 配置 单个 note
+ */
+const devexpresswpf = defineNoteConfig({
+  dir: 'devexpresswpf',
+  link: '/devexpresswpf/',
+  sidebar: [
+    '/保持原生WPF控件样式.md',
+  ]
 })
 
+
 export const notes = defineNotesConfig({
-  dir: 'notes',
+  dir: '/notes/',
   link: '/',
-  notes: [demoNote],
+  notes: [devexpresswpf]
+    
 })
