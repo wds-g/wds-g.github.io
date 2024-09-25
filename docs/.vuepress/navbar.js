c99c1f0998dabee35e7578a9126f73ccd471a235
祝辉 王 <xiluofu@163.com>
2024-09-25 22:53:48 +0800

    测试

--- docs/.vuepress/navbar.js
+++ docs/.vuepress/navbar.js
@@ -5,5 +5,8 @@ export const navbar = defineNavbarConfig([
   { text: '博客', link: '/blog/' },
   { text: '标签', link: '/blog/tags/' },
   { text: '归档', link: '/blog/archives/' },
-  { text: '笔记',link:'/notes/'},
+  {
+    text: '笔记',
+    items: [{ text: '示例', link: '/notes/demo/README.md' }]
+  },
 ])
