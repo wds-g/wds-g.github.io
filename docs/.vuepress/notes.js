6aa905ec902127eca3e13947ec7a408ae80401a9
祝辉 王 <xiluofu@163.com>
2024-09-25 22:52:19 +0800

    测试

--- docs/.vuepress/notes.js
+++ docs/.vuepress/notes.js
@@ -1,16 +1,25 @@
-import {defineNoteConfig,defineNotesConfig } from 'vuepress-theme-plume'
+import { defineNoteConfig, defineNotesConfig } from 'vuepress-theme-plume'
+
 /**
  * 配置 单个 note
  */
-const devexpresswpf = defineNoteConfig({
-  dir: 'devexpresswpf',
-  link: '/devexpresswpf/',
-  sidebar: 'auto'
+const typescript = defineNoteConfig({
+  dir: 'typescript',
+  link: '/typescript/',
+  sidebar: [
+    '/guide/intro.md',
+    '/guide/getting-start.md',
+    '/config/config-file.md',
+  ]
 })
 
-
+/**
+ * 配置 notes
+ */
 export default defineNotesConfig({
+  // 声明所有笔记的目录，(默认配置，通常您不需要声明它)
   dir: '/notes/',
   link: '/',
-  notes: [devexpresswpf]
+  // 在这里添加 note 配置
+  notes: [typescript] 
 })
